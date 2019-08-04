5613fe3e3fe4fabdc10af486aa85bf7e3c269750
Arinichev Nikolay <arinichev_n@mail.ru>
2019-08-04 08:28:41 +0300

    с

--- aon.ino
+++ aon.ino
@@ -5,7 +5,7 @@
 #define SERIAL_RATE 4800
 #define SERIAL_CONFIG SERIAL_8N1
 #define SERIAL_TIMEOUT_MS 300
-#define CHANNEL_COUNT 8
+#define CHANNEL_COUNT 2
 
 //edit this
 //for each channel
@@ -21,18 +21,18 @@ PUART puart;
 
 void setup()
 {
-  Serial.begin(SERIAL_RATE, SERIAL_CONFIG);
-  Serial.setTimeout(SERIAL_TIMEOUT_MS);
-  puart_init(&puart);
+    Serial.begin(SERIAL_RATE, SERIAL_CONFIG);
+    Serial.setTimeout(SERIAL_TIMEOUT_MS);
+    puart_init(&puart);
+    
+    //edit this
+    //for each channel
+    channel_init(&channels[0], ID1, OUT_PIN1);
+    channel_init(&channels[1], ID2, OUT_PIN2);
   
-  //edit this
-  //for each channel
-  channel_init(&channels[0], ID1, OUT_PIN1);
-  channel_init(&channels[1], ID2, OUT_PIN2);
-  
-	FOREACH_CHANNEL
-		pmem_getChannel(channel, i);
-	}
+    FOREACH_CHANNEL
+        pmem_getChannel(channel, i);
+    }
   
 }
 
